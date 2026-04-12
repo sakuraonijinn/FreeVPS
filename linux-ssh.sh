47b578941364f537d4483c464d64dbb162d47113
sakuraonijinn <sakuraonijinn@gmail.com>
2026-04-12 01:29:15 -0700

    Update ngrok installation method in linux-ssh.sh
    Replaced ngrok installation method with curl command and removed wget and unzip steps.

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -19,10 +19,12 @@ if [[ -z "$LINUX_USER_PASSWORD" ]]; then
 fi
 
 echo "### Install ngrok ###"
-
-wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
-unzip ngrok-stable-linux-386.zip
-chmod +x ./ngrok
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+  | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+  && echo "deb https://ngrok-agent.s3.amazonaws.com bookworm main" \
+  | sudo tee /etc/apt/sources.list.d/ngrok.list \
+  && sudo apt update \
+  && sudo apt install ngrok
 
 echo "### Update user: $USER password ###"
 echo -e "$LINUX_USER_PASSWORD\n$LINUX_USER_PASSWORD" | sudo passwd "$USER"
