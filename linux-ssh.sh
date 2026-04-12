6751d78ffd4be9693d638e6c4b44139556aca74d
sakuraonijinn <sakuraonijinn@gmail.com>
2026-04-12 06:17:07 -0700

    Update linux-ssh.sh

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -30,6 +30,8 @@ echo "### Update user: $USER password ###"
 echo -e "$LINUX_USER_PASSWORD\n$LINUX_USER_PASSWORD" | sudo passwd "$USER"
 
 echo "### Start ngrok proxy for 80 port ###"
+
+ngrok config add-authtoken [[ "$NGROK_AUTH_TOKEN" ]]
 # Start HTTP tunnel
 rm -f .ngrok-http.log
 ngrok http 80 --log=".ngrok-http.log" &
